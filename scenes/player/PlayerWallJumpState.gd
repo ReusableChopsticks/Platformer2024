0afb71e5f07cd6e03a91c05acdc19c34114a4d22
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2024-07-09 12:33:44 +1000

    extra wall jump height at higher speed levels

--- scenes/player/PlayerWallJumpState.gd
+++ scenes/player/PlayerWallJumpState.gd
@@ -5,9 +5,12 @@ class_name PlayerWallJumpState
 ## While in this state, the player has no control for the duration of wall_jump_time
 ## Afterwards, automatically transition back into MoveState
 
-@export var wall_jump_force: int = -300
+## Base vertical wall jump velocity
+@export var wall_jump_vel: int = -300
+## Percentage of current move speed to add on to vertical wall jump vel
+## so higher speeds mean slightly higher jumps for better game feel
+@export_range(0, 1.0) var wall_jump_vel_speed_mult: float = 0.2
 @export_range(0, 1.0) var wall_jump_time: float = 0.1
-@export_range(1, 3) var jump_release_mult: float = 2
 
 
 func exit_wall_jump():
@@ -16,8 +19,8 @@ func exit_wall_jump():
 func enter():
 	player.jump_buffer_timer.stop()
 	get_tree().create_timer(wall_jump_time).timeout.connect(exit_wall_jump)
-	player.velocity.y = wall_jump_force
-	player.velocity.x = wall_jump_force * -player.get_wall_normal().x
+	player.velocity.y = wall_jump_vel - (player.move_speed * wall_jump_vel_speed_mult)
+	player.velocity.x = player.move_speed * player.get_wall_normal().x
 
 func physics_update(delta: float):
 	player.move_and_slide()
