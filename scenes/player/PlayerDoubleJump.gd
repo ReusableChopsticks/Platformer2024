f1d1fbb7423d4e195527843a92e2ea27eb13f8af
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2024-07-11 18:08:38 +1000

    double jump state immediately transitions back to MoveState

--- scenes/player/PlayerDoubleJump.gd
+++ scenes/player/PlayerDoubleJump.gd
@@ -2,22 +2,24 @@ extends PlayerState
 class_name PlayerDoubleJumpState
 
 
-
+## Just immediately transition back into move lol
+## because thats where all the important checks for speed happen
 func enter():
 	player.jump_buffer_timer.stop()
 	player.has_double_jump = false
 	player.velocity.y = player.double_jump_vel
+	transitioned.emit(self, "PlayerMoveState")
 	
-func physics_update(delta: float):
-	# left and right movement
-	move_x(delta, player.air_move_mult)
-	apply_gravity(delta)		
-	player.move_and_slide()
-	
-	
-	if grounded():
-		transitioned.emit(self, "PlayerMoveState")
-	elif dash():
-		transitioned.emit(self, "PlayerDashState")
-	elif wall():
-		transitioned.emit(self, "PlayerWallState")
+#func physics_update(delta: float):
+	## left and right movement
+	#move_x(delta, player.air_move_mult)
+	#apply_gravity(delta)		
+	#player.move_and_slide()
+	#
+	#
+	#if grounded():
+		#transitioned.emit(self, "PlayerMoveState")
+	#elif dash():
+		#transitioned.emit(self, "PlayerDashState")
+	#elif wall():
+		#transitioned.emit(self, "PlayerWallState")
