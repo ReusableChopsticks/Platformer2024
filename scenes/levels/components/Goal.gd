17138971eac17ed4a191ee373f4d92e95bb64b79
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2024-07-13 12:35:39 +1000

    goal bobbing animation
    some polish

--- scenes/levels/components/Goal.gd
+++ scenes/levels/components/Goal.gd
@@ -1,8 +1,11 @@
-extends Area2D
+extends Node2D
 
 @onready var level: Level = get_parent()
 
-func _on_body_entered(body):
+func _ready():
+	$AnimationPlayer.play("goal_idle")
+
+func _on_area_2d_body_entered(body):
 	if body is PlayerCharacter:
 		var level_name = level.name.to_lower()
 		level.level_completed.emit(level_name)
